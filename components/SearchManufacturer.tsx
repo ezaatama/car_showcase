9f2d9b59366decdcdd6e5eb3ffd23cbcf636244e
ezaatama <ezatama33@gmail.com>
2024-08-08 09:13:44 +0700

    update selected search manufacturer

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -15,7 +15,7 @@ const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerP
     item.toLowerCase().replace(/\s+/g, "").includes(query.toLowerCase().replace(/\s+/g, ""))))
   return (
     <div className='search-manufacturer'>
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className='relative w-full'>
           <Combobox.Button className='absolute top-[14px]'>
             <Image 
@@ -46,7 +46,20 @@ const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerP
                     className={({ active }) => `relative search-manufacturer__option ${active ? 'bg-primary-blue text-white' : 'text-gray-900'}`}
                     value={item}
                   >
-                    {item}
+                    {({ selected, active }) => (
+                      <>
+                        <span className={`block truncate ${
+                          selected ? 'font-medium' : 'font-normal'
+                        }`}>
+                          {item}
+                        </span>
+                        { selected ? <span className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active ? 'text-white': 'text-teal-600' }`}>
+
+                        </span> : null
+
+                        }
+                      </>
+                      )}
                   </Combobox.Option>
                 ))
               }
